f3d3b25e09e5c35e24c6b6d6edef5555f5ecfe41
Simeon Griggs <simeon@hey.com>
2022-11-21 13:02:34 +0000

    fix: show full reference field preview

--- src/components/InternationalizedField.tsx
+++ src/components/InternationalizedField.tsx
@@ -1,30 +1,13 @@
-import {SyncIcon} from '@sanity/icons'
-import {Box, Button, Flex} from '@sanity/ui'
-import React, {useCallback} from 'react'
-import {FieldProps, unset, useFormBuilder} from 'sanity'
+import {FieldProps} from 'sanity'
 
 export default function InternationalizedField(props: FieldProps) {
-  const {renderPreview} = useFormBuilder()
-  const handleUnset = useCallback(() => {
-    props.inputProps.onChange(unset())
-  }, [props.inputProps])
-
   // Show reference field selector if there's a value
   if (props.schemaType.name === 'reference' && props.value) {
-    // Shows just the preview, but not the button to remove the value
-    // TODO: This is a stop-gap, restore default reference UI behaviour
-    return (
-      <Flex gap={2}>
-        <Box flex={1}>
-          {renderPreview({
-            layout: 'default',
-            schemaType: props.schemaType,
-            value: props.value,
-          })}
-        </Box>
-        <Button mode="ghost" icon={SyncIcon} onClick={handleUnset} />
-      </Flex>
-    )
+    return props.renderDefault({
+      ...props,
+      title: '',
+      level: 0,
+    })
   }
 
   return props.children
