bd4d39555316f067f944045ad9b272d2c008cef9
Simeon Griggs <simeon@hey.com>
2023-06-01 13:45:18 +0100

    chore: linting

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {definePlugin} from 'sanity'
+
 import Preload from './components/Preload'
 import array from './schema/array'
 import object from './schema/object'
@@ -10,29 +11,38 @@ const CONFIG_DEFAULT: PluginConfig = {
   fieldTypes: [],
 }
 
-export const internationalizedArray = definePlugin<PluginConfig>((config = CONFIG_DEFAULT) => {
-  const {apiVersion = '2022-11-27', select, languages, fieldTypes} = {...CONFIG_DEFAULT, ...config}
+export const internationalizedArray = definePlugin<PluginConfig>(
+  (config = CONFIG_DEFAULT) => {
+    const {
+      apiVersion = '2022-11-27',
+      select,
+      languages,
+      fieldTypes,
+    } = {...CONFIG_DEFAULT, ...config}
 
-  return {
-    name: 'sanity-plugin-internationalized-array',
-    // If `languages` is a callback then let's preload it
-    studio: Array.isArray(languages)
-      ? undefined
-      : {
-          components: {
-            layout: (props) => (
-              <>
-                <Preload apiVersion={apiVersion} languages={languages} />
-                {props.renderDefault(props)}
-              </>
-            ),
+    return {
+      name: 'sanity-plugin-internationalized-array',
+      // If `languages` is a callback then let's preload it
+      studio: Array.isArray(languages)
+        ? undefined
+        : {
+            components: {
+              layout: (props) => (
+                <>
+                  <Preload apiVersion={apiVersion} languages={languages} />
+                  {props.renderDefault(props)}
+                </>
+              ),
+            },
           },
-        },
-    schema: {
-      types: [
-        ...fieldTypes.map((type) => array({type, apiVersion, select, languages})),
-        ...fieldTypes.map((type) => object({type})),
-      ],
-    },
+      schema: {
+        types: [
+          ...fieldTypes.map((type) =>
+            array({type, apiVersion, select, languages})
+          ),
+          ...fieldTypes.map((type) => object({type})),
+        ],
+      },
+    }
   }
-})
+)
